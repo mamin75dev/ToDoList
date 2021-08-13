ebb8788cc54ce5bf1766347ee18a76e33562b598
mohamadamin <mamin75.dev@gmail.com>
2021-08-13 17:36:09 +0430

    frontend first screens and components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Header } from "./components";
+import { Home, Login } from "./screens";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "./fonts/Samim/Samim-Bold-FD-WOL.ttf";
 
-function App() {
+export default () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Switch>
+        <Route path="/home">
+          <Home />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/">
+          <Login />
+        </Route>
+      </Switch>
+    </Router>
   );
-}
-
-export default App;
+};
---
+++ frontend/src/components/Header/index.js
@@ -0,0 +1,4 @@
+import './index.css';
+export const Header = () => {
+  return <header className="App-header" />;
+};
